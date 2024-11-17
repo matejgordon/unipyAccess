6f5bb0d25749e5cd608a4053782813cfa9f2b7f2
Matěj Gordon <gordon.matej@gmail.com>
2024-11-17 20:36:14 +0100

    Added set_access_method function

--- unipyAccess/hardware.py
+++ unipyAccess/hardware.py
@@ -9,6 +9,10 @@ class HardwareManager:
         endpoint = "/proxy/access/api/v2/devices"
         return self.api_client.get(endpoint)
     
+    def get_device(self, device_id):
+        endpoint = f"/proxy/access/api/v2/device/{device_id}"
+        return self.api_client.get(endpoint)
+    
     def restart_device(self, device_id):
         endpoint = f"/proxy/access/api/v2/device/{device_id}/restart"
         return self.api_client.post(endpoint)
@@ -17,23 +21,31 @@ class HardwareManager:
         endpoint = f"/proxy/access/api/v2/device/{device_id}/configs"
         payload = []
 
-        if "nfc" in enabled_methods:
-            payload.append({"key":"nfc","tag":"open_door_mode","value":"yes"})
-        elif "nfc" not in enabled_methods:
-            payload.append({"key":"nfc","tag":"open_door_mode","value":"no"})
-        if "wave" in enabled_methods:
-            payload.append({"key":"wave","tag":"open_door_mode","value":"yes"})
-        elif "wave" not in enabled_methods:
-            payload.append({"key":"wave","tag":"open_door_mode","value":"no"})
-        if "mobile_button" in enabled_methods:
-            payload.append({"key":"bt_button","tag":"open_door_mode","value":"yes"})
-        elif "mobile_button" not in enabled_methods:
-            payload.append({"key":"bt_button","tag":"open_door_mode","value":"no"})
+        methods_map = {
+            "nfc": "nfc",
+            "wave": "wave",
+            "mobile_button": "bt_button",
+            "mobile_tap": "bt_tap",
+        }
+
+        for method, key in methods_map.items():
+            value = "yes" if method in enabled_methods else "no"
+            payload.append({"key": key, "tag": "open_door_mode", "value": value})
 
         if "nfc" in enabled_methods and "wave" in enabled_methods:
-            logging.error('Cannot enable both NFC and Wave')
+            logging.error("Cannot enable both NFC and Wave")
             sys.exit(1)
 
-        print(payload)
+        device_capabilities = self.get_device(device_id)['data']['capabilities']
+        
+        if "pin" in enabled_methods and "pin_code" not in device_capabilities:
+            logging.error("PIN is not supported on this device")
+            sys.exit(1)
 
-        return self.api_client.put(endpoint, payload)
+        if "wave" in enabled_methods and "sensitivity" not in device_capabilities:
+            logging.error("Wave is not supported on this device")
+            sys.exit(1)
+
+        response = self.api_client.put(endpoint, payload)
+        logging.info(f"Set access method for device {device_id} to {enabled_methods}")
+        return response
